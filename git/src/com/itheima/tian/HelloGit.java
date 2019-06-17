67c64aeeda5495cd735e8eec8e90a063092d1783
Jiang <1683441507@qq.com>
2019-06-17 09:57:08 +0800

    把本地工程添加到git

---
+++ git/src/com/itheima/tian/HelloGit.java
@@ -0,0 +1,14 @@
+package com.itheima.tian;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("git来了");
+        System.out.println("git来了");
+        System.out.println("git来了");
+        System.out.println("git来了");
+        System.out.println("git来了");
+        System.out.println("git来了");
+
+    }
+
+}
